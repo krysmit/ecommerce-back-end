854ac27a265cacad653920d20dcca560c425c1b3
Krystyn Smith <krskrsmith@gmail.com>
2021-10-22 18:45:05 -0500

    updated the product tag js and the tags js

--- models/ProductTag.js
+++ models/ProductTag.js
@@ -7,6 +7,15 @@ class ProductTag extends Model {}
 ProductTag.init(
   {
     // define columns
+    id: {
+      type: DataTypes.INTEGER
+    },
+    product_id: {
+      type: DataTypes.INTEGER
+    },
+    tag_id: {
+      type: DataTypes.INTEGER
+    },
   },
   {
     sequelize,
