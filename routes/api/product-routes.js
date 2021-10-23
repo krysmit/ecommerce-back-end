f79a7bddc3f82c6a13f112904ba0fad171e648b5
Krystyn Smith <krskrsmith@gmail.com>
2021-10-22 23:41:38 -0500

    working to add in api routing

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -7,12 +7,34 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 router.get('/', (req, res) => {
   // find all products
   // be sure to include its associated Category and Tag data
+  try {
+    const locationData = await Product.findAll({
+      include: [{ model: Category }, { model: Tag }]
+    });
+    res.status(200).json(locationData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
+
+
 
 // get one product
 router.get('/:id', (req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
+  try {
+    const locationData = await Product.findByPk(req.param.id, {
+      include: [{ model: Category }, {model: Tag}]
+    });
+    if(!locationData) {
+      res.status(404).json({ message: 'Product with this id not recognized.' });
+      return;
+    }
+    res.status(200).json(locationData);
+    } catch(err) {
+      res.status(500).json(err);
+    }
 });
 
 // create new product
@@ -25,7 +47,13 @@ router.post('/', (req, res) => {
       tagIds: [1, 2, 3, 4]
     }
   */
-  Product.create(req.body)
+  Product.create({
+    product_name: req.body.product_name,
+    price: req.body.price,
+    stock: req.body.stock,
+    category_id: req.body.category_id,
+    tagIds: req.body.tagIds
+  })
     .then((product) => {
       // if there's product tags, we need to create pairings to bulk create in the ProductTag model
       if (req.body.tagIds.length) {
