0d9330a6b5e95251c28c9f442845d0be4e3fde10
Krystyn Smith <krskrsmith@gmail.com>
2021-10-23 00:00:45 -0500

    beginning api routing for categories

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -6,15 +6,43 @@ const { Category, Product } = require('../../models');
 router.get('/', (req, res) => {
   // find all categories
   // be sure to include its associated Products
+  try {
+    const locationData = await Category.findAll({
+      include: [{ model: Product }]
+    });
+    res.status(200).json(locationData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.get('/:id', (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
+  try {
+    const locationData = await Category.findByPk(req.params.id, {
+      include: [{ model: Product }]
+    });
+
+    if (!locationData) {
+      res.status(404).json({ message: 'No category found with this id!' });
+      return;
+    }
+
+    res.status(200).json(locationData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.post('/', (req, res) => {
   // create a new category
+  try {
+    const categoryData = await Category.create(req.body);
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(400).json(err);
+  }
 });
 
 router.put('/:id', (req, res) => {
